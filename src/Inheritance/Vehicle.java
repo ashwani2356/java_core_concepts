bf4849dbf937b7d42fac77c788f222596d3568cb
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-02-14 09:56:40 +0530

    practice inheritance,this,super keyword questions

--- src/Inheritance/Vehicle.java
+++ src/Inheritance/Vehicle.java
@@ -4,13 +4,14 @@ class Vehicle {
     int wheelsCount;
     String colour;
     String name;
-    Vehicle(String name,int wheelsCount,String colour){
+    final String partner="motoroctan";
+     Vehicle(String name,int wheelsCount,String colour){
         this.wheelsCount=wheelsCount;
         this.name=name;
         this.colour=colour;
         System.out.println("name: "+name+" ,wheelsCount: "+wheelsCount+" ,colour: "+colour);
     }
-    void start(){
+    final void start(){
         System.out.println("vehicle is starting");
     }
 
